32f56179e1715e894265973197644b625b681f24
URJCheca <shijimaginkoss@gmail.com>
2019-03-18 21:01:29 +0100

    Servicio interno

---
+++ src/main/java/com/dad/serviciorest/ServiciorestApplication.java
@@ -0,0 +1,18 @@
+package com.dad.serviciorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciorestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciorestApplication.class, args);
+		String destinatario =  "alexisasi12@hotmail.com"; //A quien le quieres escribir.
+	    String asunto = "Correo de prueba enviado desde Java";
+	    String cuerpo = "Esta es una prueba de correo...";
+
+	    EmailSenderService.enviarConGMail(destinatario, asunto, cuerpo);
+	}
+
+}
